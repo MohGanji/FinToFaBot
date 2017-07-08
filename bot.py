0610416148f7cafe59c62a08ce60c0043223cbf3
root <root@etuts.ir>
2017-07-08 10:15:16 +0430

    updated way of getting token

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
+import token
 
-bot  = telebot.TeleBot("Token")
+TOKEN = token()
+bot  = telebot.TeleBot(TOKEN.get_token())
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
