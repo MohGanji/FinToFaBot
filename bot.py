5582db821756f7cfd769c7b5538fa5402b154dea
root <root@etuts.ir>
2017-07-08 11:03:45 +0430

    test

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import telebot
 import token
 
-TOKEN = token()
+TOKEN = token.token()
 bot  = telebot.TeleBot(TOKEN.get_token())
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-	pass
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+bot.polling()
