8053118d23577afd9e0ad7fb652ca085821c027d
MohGanji <mohammad-ganji@outlook.com>
2017-07-09 13:04:50 +0430

    debug indents

--- bot.py
+++ bot.py
@@ -17,46 +17,46 @@ telebot.logger.setLevel(logging.INFO)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
-                  or just send me a message"))
+	bot.reply_to(message,
+				 ("Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
+				  or just send me a message"))
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
-                  or just send me a message"))
+	bot.reply_to(message,
+				 ("Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
+				  or just send me a message"))
 
 def transliterate(message):
 	text = message.reply_to_message.text
-    # if((text == "enable" or text == "disable")) and (is_user(message.chat) == True):
-    #     return
-    user_id = message.from_user.id
-    logging.critical(str(user_id)+" : "+text)
-    if text:
-        if (text[0] == '/'):
-            text = text[1:]
-        text = text.replace("@TransliterateBot", "")
-        text = text.split()
-        # user_dictionary = db["user_dicts"].find_one({"user_id": user_id})
-        # if(user_dictionary is not None):
-        #     text = use_dict(text,user_dictionary["words"])
-        # pref = db["user_prefs"].find_one({'user_id': user_id})
-        # if(pref is None):
-        #     db["user_prefs"].update({'user_id': message.from_user.id}, {'$setOnInsert': {'user_id': message.from_user.id, "denahal": False, "filter": True, "parsi": False}}, upsert = True)
-        #     pref = db["user_prefs"].find_one({'user_id': user_id})
-        # if(pref["denahal"]):
-        #     text = denahalize(text)
-        # if(pref["filter"]):
-        #     text = filter_text(text)
-        # if(pref["parsi"]):
-        #     text = parsi_text(text)
-        # text = global_replaces(text)
-        shcommand = ['php', './behnevis.php']
-        shcommand.extend(text)
-        p = Popen(shcommand, stdout=PIPE, stderr=PIPE)
-        text, err = p.communicate()
-        bot.reply_to(message, text)
+	# if((text == "enable" or text == "disable")) and (is_user(message.chat) == True):
+	#     return
+	user_id = message.from_user.id
+	logging.critical(str(user_id)+" : "+text)
+	if text:
+		if (text[0] == '/'):
+			text = text[1:]
+		text = text.replace("@TransliterateBot", "")
+		text = text.split()
+		# user_dictionary = db["user_dicts"].find_one({"user_id": user_id})
+		# if(user_dictionary is not None):
+		#     text = use_dict(text,user_dictionary["words"])
+		# pref = db["user_prefs"].find_one({'user_id': user_id})
+		# if(pref is None):
+		#     db["user_prefs"].update({'user_id': message.from_user.id}, {'$setOnInsert': {'user_id': message.from_user.id, "denahal": False, "filter": True, "parsi": False}}, upsert = True)
+		#     pref = db["user_prefs"].find_one({'user_id': user_id})
+		# if(pref["denahal"]):
+		#     text = denahalize(text)
+		# if(pref["filter"]):
+		#     text = filter_text(text)
+		# if(pref["parsi"]):
+		#     text = parsi_text(text)
+		# text = global_replaces(text)
+		shcommand = ['php', './behnevis.php']
+		shcommand.extend(text)
+		p = Popen(shcommand, stdout=PIPE, stderr=PIPE)
+		text, err = p.communicate()
+		bot.reply_to(message, text)
 
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
