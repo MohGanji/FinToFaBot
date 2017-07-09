84eb8e9f4daeb24696ae76d1bebb43b196ef0669
MohGanji <mohammad-ganji@outlook.com>
2017-07-09 11:01:27 +0430

    trying to set up webhook

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import telebot
 import Token
 import flask
@@ -6,41 +9,37 @@ import logging
 TOKEN = Token.token()
 bot  = telebot.TeleBot(TOKEN.get_token())
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 # This is a simple echo bot using decorators and webhook with flask
 # It echoes any incoming text messages and does not use the polling method.
 
+########## az inja be payeen copy shode
 
+API_TOKEN = '<api_token>'
 
-API_TOKEN = 'mohammad/toFarsiBot/bot.py'
-
-WEBHOOK_HOST = "vmoh.ir"
+WEBHOOK_HOST = 'etuts.ir'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-# WEBHOOK_SSL_CERT = ''  # Path to the ssl certificate
-# WEBHOOK_SSL_PRIV = ''  # Path to the ssl private key
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
-# # Quick'n'dirty SSL certificate generation:
-# #
-# # openssl genrsa -out webhook_pkey.pem 2048
-# # openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-# #
-# # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# # with the same value in you put in WEBHOOK_HOST
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (TOKEN)
+WEBHOOK_URL_PATH = "/%s/" % (TOKEN.get_token())
 
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
-bot = telebot.TeleBot(TOKEN)
-
 app = flask.Flask(__name__)
+
 
 # Empty webserver index, return nothing, just http 200
 @app.route('/', methods=['GET', 'HEAD'])
