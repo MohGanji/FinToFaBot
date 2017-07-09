48c31866bd2e6adea5a8cde9bdc66e5b0971be5f
MohGanji <mohammad-ganji@outlook.com>
2017-07-09 12:15:47 +0430

    trying to set up webhook

--- bot.py
+++ bot.py
@@ -16,13 +16,41 @@ telebot.logger.setLevel(logging.INFO)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-                  "I am here to echo your kind words back to you."))
+                 ("Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
+                  or just send me a message"))
 
 
 # Handle all other messages
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def transliterate(message):
+    text = message.text
+    # if((text == "enable" or text == "disable")) and (is_user(message.chat) == True):
+    #     return
+    user_id = message.from_user.id
+    logging.critical(str(user_id)+" : "+text)
+    if text:
+        if (text[0] == '/'):
+            text = text[1:]
+        text = text.replace("@TransliterateBot", "")
+        text = text.split()
+        # user_dictionary = db["user_dicts"].find_one({"user_id": user_id})
+        # if(user_dictionary is not None):
+        #     text = use_dict(text,user_dictionary["words"])
+        # pref = db["user_prefs"].find_one({'user_id': user_id})
+        # if(pref is None):
+        #     db["user_prefs"].update({'user_id': message.from_user.id}, {'$setOnInsert': {'user_id': message.from_user.id, "denahal": False, "filter": True, "parsi": False}}, upsert = True)
+        #     pref = db["user_prefs"].find_one({'user_id': user_id})
+        # if(pref["denahal"]):
+        #     text = denahalize(text)
+        # if(pref["filter"]):
+        #     text = filter_text(text)
+        # if(pref["parsi"]):
+        #     text = parsi_text(text)
+        # text = global_replaces(text)
+        shcommand = ['php', './behnevis.php']
+        shcommand.extend(text)
+        p = Popen(shcommand, stdout=PIPE, stderr=PIPE)
+        text, err = p.communicate()
+        bot.reply_to(message, text)
 
 bot.polling()
