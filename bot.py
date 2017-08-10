6519c29d53700da6a137461a23fb35e358c7c7ca
MohGanji <mohammad-ganji@outlook.com>
2017-08-10 11:55:11 +0430

    method changed to webhook

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@
 import telebot
 import Token
 import logging
+import flask
 from subprocess import (PIPE, Popen)
 
 
@@ -13,6 +14,38 @@ bot  = telebot.TeleBot(TOKEN.get_token())
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
+#########
+# webhook preparation
+
+WEBHOOK_HOST = '192.241.169.182'
+WEBHOOK_PORT = '8443'
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_SSL_CERT = "./webhook_cert.pem"
+WEBHOOK_PRIV_CERT = "./webhook_pkey.pem"
+
+WEBHOOK_URL_BASE = 'https://%s:%s' % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (TOKEN)
+
+router = flask.Flask(__name__)
+
+@router.route('/', methods=['GET', 'HEAD'])
+def index():
+	return ''
+
+@router.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+	if flask.request.headers.get('content-type') == 'application/json':
+		json_string = flask.request.get_data.decode('utf-8')
+		update = bot.types.Update.de_json(json_string)
+		bot.process_new_updates([update])
+		return ''
+	else
+		flask.abort(403)
+
+
+
+##########
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
@@ -50,7 +83,7 @@ def handle_group_or_user(message):
 	if message.chat.type == "private":
 		transliterate_to_farsi(message)
 	else:
-		if message.text == 'fa' or message.text == 'فا'.decode('utf-8'):
+		if message.text == 'fa' or message.text == 'Fa' or message.text == 'فا'.decode('utf-8'):
 			msg = message.reply_to_message
 			if msg is not None:
 				transliterate_to_farsi(msg)
@@ -58,4 +91,10 @@ def handle_group_or_user(message):
 				logging.critical("Err : message is empty")
 
 
-bot.polling()
+# bot.polling()
+
+bot.remove_webhook()
+
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+router.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_PRIV_CERT)), debug=True)
