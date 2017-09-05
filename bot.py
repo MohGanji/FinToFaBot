e171f4ab78e3dde251358e02d2d5c3f4d4dc1286
MohGanji <mohammad-ganji@outlook.com>
2017-09-05 17:32:02 +0430

    back to previous wrapper, sticking to getUpdates method :/

--- bot.py
+++ bot.py
@@ -2,72 +2,81 @@
 # -*- coding: utf-8 -*-
 
 ##### IMPORTS ######
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
 import Token
 import logging
+import flask
 import time
 from subprocess import (PIPE, Popen)
 ####################
 
 ## CONST MESSAGES ##
-START_MESSAGE = "Hi, all you need to do is add me to a group and then reply 'fa or فا' to any message and I will transliterate it for you.\n\
+START_MESSAGE = "Hi, all you need to do is add me to a group and\
+             then reply 'fa or فا' to any message and I will transliterate it for you.\n\
 				or just send me a message"
 
+HELP_MESSAGE = "I WILL ADD A HELP MESSAGE SOON"
 
 ####################
 
+TOKEN = Token.token()
+bot = telebot.TeleBot(TOKEN.get_token())
 
-TOKEN = Token.token().get_token()
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
-
-##### COMMANDS #####
-def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=START_MESSAGE)
-
-####################
-
-#### FUNCTIONS #####
+# Handle '/start' and '/help'
 
 
-def transliterate(message):
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    """ function for start command """
+    bot.reply_to(message,
+                 (START_MESSAGE))
+
+
+@bot.message_handler(commands=['help'])
+def help_provider(message):
+    """ function for help command """
+    bot.reply_to(message,
+                 (HELP_MESSAGE))
+
+
+def transliterate_to_farsi(message):
+    """ transliterate finglish messages to farsi """
     text = message.text
-    user_id = message.user.id
+    user_id = message.from_user.id
     logging.critical(str(user_id) + " : " + text)
     if text:
-        if (text[0] == '/'):
+        if text[0] == '/':
             text = text[1:]
+
         text = text.replace("@TransliterateBot", "")
         text = text.split()
         shcommand = ['php', './behnevis.php']
         shcommand.extend(text)
-        p = Popen(shcommand, stdout=PIPE, stderr=PIPE)
-        text, err = p.communicate()
-        if err:
-            logging.critical("PHP ERR: " + err)
+        pipe = Popen(shcommand, stdout=PIPE, stderr=PIPE)
+        text, err = pipe.communicate()
+        logging.critical("PHP ERR: " + err)
         logging.critical("res : " + str(user_id) + " : " + text)
         return text
 
-
-def finToFa(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=transliterate(update.message))
+# Handle all other messages
 
 
-####################
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_group_or_user(message):
+    """ check if message is sent to the bot or in a group """
+    if message.chat.type == "private":
+        transliterate_to_farsi(message)
+    else:
+        if message.text == 'fa' or message.text == 'Fa' or message.text == 'فا'.decode('utf-8'):
+            msg = message.reply_to_message
+            if msg is not None:
+                text = transliterate_to_farsi(msg)
+                bot.reply_to(msg, text)
+            else:
+                logging.critical("Err : message is empty")
 
-##### HANDLERS #####
-start_handler = CommandHandler('start', startCommand)
-dispatcher.add_handler(start_handler)
 
-finToFa_handler = MessageHandler(Filters.text, finToFa)
-dispatcher.add_handler(finToFa_handler)
-####################
-
-updater.start_webhook(listen='0.0.0.0',
-                      port=8443,
-                      url_path=TOKEN,
-                      webhook_url='https://webhook.mohganji.ir:8443/' + TOKEN)
+bot.polling()
