e1f9a3938b0f586c3e4c2f7236d37ef2cb191241
MohGanji <mohammad-ganji@outlook.com>
2017-09-10 04:03:24 +0430

    urgent bug fix

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def addNewUser(db, username, chatId):
     new_user = {'id': chatId, 'username': username}
     if not db.users.find_one({"id" : new_user["id"]}):
-        db.users.insert_one()
+        db.users.insert_one(new_user)
     else:
         logging.critical("db: " + str(new_user) + " : " + "user exists!")
